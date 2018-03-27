0febfb142783789ebe55bf99d19d91f1dfe9b04c
vctm4266 <Redempteur1!>
2018-03-27 15:51:12 +0200

    modify mapping file name while receive and save

--- open_data_injector_thread.py
+++ open_data_injector_thread.py
@@ -44,7 +44,7 @@ class ThreadClass(Thread):
 
         try:
 
-            if not os.path.exists(MAPPING_PATH):
+            if os.path.exists(MAPPING_PATH):
                 try:
                     with open(os.path.join(MAPPING_PATH), "r") as f:
                         mapping = json.load(f)
